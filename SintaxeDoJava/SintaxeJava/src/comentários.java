f571642b2472a221ea7304d607ff9880cf3f79e0
Vicrt0rMartinelli <josevictordasm17112006@gmail.com>
2025-10-22 09:53:30 -0300

    qqw

--- SintaxeDoJava/SintaxeJava/src/comentários.java
+++ SintaxeDoJava/SintaxeJava/src/comentários.java
@@ -1,5 +1,5 @@
 public class comentários {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
      
         //Cometário em uma única linha
         /*
---
+++ teste/src/teste000112.java
@@ -0,0 +1,6 @@
+public class teste000112 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Helaksjdhgjdsalo, World!");
+        System.out.println("Hello, World!");
+    }
+}
