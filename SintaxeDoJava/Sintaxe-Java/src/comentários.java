7da6df779b1e462b2cf480d8dd4b897ed2f820b7
Vicrt0rMartinelli <josevictordasm17112006@gmail.com>
2025-10-22 09:24:03 -0300

    salve

---
+++ SintaxeDoJava/Sintaxe-Java/src/comentários.java
@@ -0,0 +1,23 @@
+public class comentários {
+    public static void main(String[] args) {
+     
+        //Cometário em uma única linha
+        /*
+         * sddcs
+         * cssd
+         * cssc
+         * comentário mais ropusto
+         *  
+         * 
+         */
+
+    }
+    /**
+         * Legal esse tipo de comentário
+         * teste de linhas 
+         * abcd3
+         */
+    public void metodo() {
+        
+    }
+}
