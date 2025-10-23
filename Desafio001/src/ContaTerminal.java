d25bd407144a0269062a75b0e38196bb246acd6b
Vicrt0rMartinelli <josevictordasm17112006@gmail.com>
2025-10-23 19:47:11 -0300

    salva ia

---
+++ Desafio001/src/ContaTerminal.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+public static void main(String [] args){
+
+    Locale brasil = new Locale ("pt","BR");
+    Scanner scanner = new Scanner (System.in).useLocale(brasil);
+
+    System.out.println("Nome: ");
+    String nome = scanner.nextLine();
+
+    System.out.println("Numero da agencia: ");
+    String agencia= scanner.nextLine();
+
+    System.out.println("Numero da conta: ");
+    int conta= scanner.nextInt();
+
+    System.out.println("Saldo: (separar com virgula !!!) ");
+    Double saldo = scanner.nextDouble(); 
+
+    /*System.out.println("Meu nome é " + nome);
+    System.out.println("Numero da agencia é " + agencia); 
+    System.out.println("Numero da conta é " + conta);
+    System.out.printf("Meu saldo é R$ %.2f\\n",saldo);*/
+
+    System.out.printf("Olá %s obrigado por criar uma conta em nosso banco, sua agência é %s, %d e seu saldo R$ %.2f já está disponível para saque",nome, agencia , conta, saldo);
+
+    scanner.close();
+}    
+}
