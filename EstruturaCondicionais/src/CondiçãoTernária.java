22ffc1800d3ce01e584b90ca8922c03c8307d075
Vicrt0rMartinelli <josevictordasm17112006@gmail.com>
2025-10-25 14:20:06 -0300

    as

--- EstruturaCondicionais/src/CondiçãoTernária.java
+++ EstruturaCondicionais/src/CondiçãoTernária.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class CondiçãoTernária {
-    
+    public static void main (String []args){
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Qual é o tamanho em cm????");
+        Double tamanho = scanner.nextDouble();
+
+        String resultado = (tamanho >= 18) ? "Nossa lobio mal!!!" : 
+                            (tamanho >= 12.5) ? "A média não é uma motivo de vergonha" : 
+                           "Tudo pequenininho aí?";
+        System.out.println(resultado);
+        scanner.close();
+    }
 }
